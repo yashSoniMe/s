9101be75b67a28ed41c834512e184a523da6fed3
yashSoniMe <yashSoniMe>
2025-08-30 12:26:09 +0530

    Made the basic of main.dart & added font

---
+++ lib/Widgets/chooser.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class chooser extends StatefulWidget {
+  const chooser({super.key});
+
+  @override
+  State<chooser> createState() => _chooserState();
+}
+
+class _chooserState extends State<chooser> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
